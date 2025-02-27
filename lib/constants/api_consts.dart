54fb4501a69ce53833aa7845fd60c8d06ab55f54
Shayan Poursadeghi <sh.psadeghi@gmail.com>
2025-02-27 15:40:16 +0330

    project updated - rest api - credentials added to env file

---
+++ lib/constants/api_consts.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class ApiConsts {
+  static String BASE_URL = '' ?? "https://api.openai.com/v1";
+  static String API_KEY = '' ?? "abc-api-key";
+}
+
